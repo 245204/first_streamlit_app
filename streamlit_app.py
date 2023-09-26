5d9baf46e6d7dd5b0884330ab685372ff087a0a8
245204 <127503000+245204@users.noreply.github.com>
2023-09-26 15:50:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+
 streamlit.title('Breakfast Menu')
 import streamlit
 streamlit.markdown('Omega 3 & Blueberry Oatmeal')
-streamlit.markdown('Kale,Spinach & Rocket Smoothie')
-streamlit.markdown('Hard-Boiled Free-Range Egg')
+# import streamlit
+# streamlit.markdown('Kale,Spinach & Rocket Smoothie')
+# import streamlit
+# streamlit.markdown('Hard-Boiled Free-Range Egg')
