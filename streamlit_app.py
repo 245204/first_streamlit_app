1ca4fdc83d3ba7ee46f6118cb18bce8200e5238d
245204 <127503000+245204@users.noreply.github.com>
2023-09-27 18:01:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,13 +39,13 @@ try:
 except URLError as e:
   streamlit.error()
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
+streamlit.header("The fruit Load list contains:")
 #Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnz.cursor() as my_cur:
     my_cur.exceute("insert into fruit_load_list values('from streamlit')")
-    return "Thanks for adding"+new_fruit
-add_my_fruit=streamlit.text_input('What fruit would you like to add?")
+    return "Thanks for adding" + new_fruit
+add_my_fruit= streamlit.text_input('What fruit would you like to add?")
 if streamlit.button('Add a fruit to the list'):
                                   my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
                                   back_from_function=insert_row_snowflake(add_my_fruit)
@@ -53,16 +53,16 @@ if streamlit.button('Add a fruit to the list'):
 #streamlit.write('Than\ks for adding',add_my_fruit)
 
 #don't run anything past here while we troubleshoot
-streamlit.stop();
+# streamlit.stop();
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select*from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("select*from fruit_load_list")
+# my_data_rows = my_cur.fetchall()
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_rows)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Jackfruit')
-streamlit.write('The user entered ', fruit_choice)
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+# fruit_choice = streamlit.text_input('What fruit would you like information about?','Jackfruit')
+# streamlit.write('The user entered ', fruit_choice)
+# my_cur.execute("insert into fruit_load_list values('from streamlit')")
